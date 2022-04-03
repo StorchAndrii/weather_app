1b7f3210694f220696242edba57d44de44c51eee
StorchAndrii <andrey.st.light@gmail.com>
2022-04-03 21:27:21 +0300

    Lesson10--useReducer

--- src/Input/index.js
+++ src/Input/index.js
@@ -1,42 +1,29 @@
-import React, { useState, useRef, useMemo } from "react";
+import React, { useState, useRef } from "react";
 
-export const Input = ({ setCitiesList }) => {
+export const Input = ({ dispatch }) => {
   const [inputValue, setInputValue] = useState("");
-  const [someValue, setSomeValue] = useState(0);
 
   const inputRef = useRef(null);
 
   const handleOnClick = () => {
-    // console.log("inputRef", inputRef);
-    setCitiesList((currentArray) => [...currentArray, inputValue]);
-    setInputValue("");
-    inputRef.current.focus();
+    if (inputValue.length) {
+      dispatch({
+        type: "ADD_CITY",
+        payload: inputValue,
+      });
+      setInputValue("");
+      inputRef.current.focus();
+    }
   };
 
   const handleOnChange = (event) => {
-    // console.log(event);
     setInputValue(event.target.value);
   };
-
-  const onSomeClick = (event) => {
-    setSomeValue((someValue) => someValue + 1);
-  };
-
-  const oldInputValue = useMemo(
-    () => `${inputValue}_${Math.random()}`,
-    [inputValue]
-  );
-
-  // console.log("someValue", someValue);
-  // console.log("oldInputValue", oldInputValue);
-  // console.log("from render", `${inputValue}_${Math.random()}`);
-  // console.log("inputValue", inputValue);
 
   return (
     <div className="InputWrap">
       <input
         className="Input"
-        onClick={onSomeClick}
         onChange={handleOnChange}
         value={inputValue}
         ref={inputRef}
